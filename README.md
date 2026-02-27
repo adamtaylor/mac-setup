bb15dcee605ea69c2c1963bf343d070ad7498517
Adam Taylor <adam@adzuna.com>
2026-02-27 17:06:20 +0000

    Start of mac setup repo

---
+++ README.md
@@ -0,0 +1,1 @@
+Install: curl -fsSL https://raw.githubusercontent.com/username/repo/main/bootstrap.sh | bash
---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+# Bootstrap script for setting up a new OS X machine
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
