c7c412073999e293d09630f864fba474825675ac
Adam Taylor <adam@adzuna.com>
2026-03-02 10:53:46 +0000

    Tweak mac settings

---
+++ mac-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Configuring macOS settings..."
+
+# Show Bluetooth menu bar icon
+defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool true
+
+# Disable natural scroll direction
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Enable trackpad tap to click (for current user)
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Automatically hide and show the dock
+defaults write com.apple.dock autohide -bool true
+
+# Restart affected services to apply changes immediately
+killall SystemUIServer
+killall Dock
+
+echo "Done. You may need to log out and back in for all changes to take effect."
